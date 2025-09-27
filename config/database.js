647fded95f116604932fcf74782c0be0e5c8218f
kaifdev01 <kaiftanveer943@gmail.com>
2025-09-27 19:09:31 +0500

    Update database.js

--- config/database.js
+++ config/database.js
@@ -5,10 +5,7 @@ const connectDB = async () => {
     const conn = await mongoose.connect('mongodb+srv://admin:admin@cluster0.aewgwmo.mongodb.net/test?retryWrites=true&w=majority', {
       useNewUrlParser: true,
       useUnifiedTopology: true,
-      maxPoolSize: 10, // Limit connection pool
-      serverSelectionTimeoutMS: 5000,
-      socketTimeoutMS: 45000,
-      bufferCommands: false,
+     
     });
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
